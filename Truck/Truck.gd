6f697693d3ffa46585f7219354c5c8de1bce44e3
Lorenzo Ferri <lorenzo.ferri96@gmail.com>
2025-10-26 21:09:07 +0100

    Test network interpolation

--- Truck/Truck.gd
+++ Truck/Truck.gd
@@ -10,13 +10,16 @@ extends VehicleBody3D
 @onready var camera_arm: SpringArm3D = $CameraArm
 @onready var rear_left_gpu_particles: GPUParticles3D = $RearLeftGPUParticles
 
+@export var network_transform: Transform3D
+@export var network_quaternion: Quaternion
+
 func _ready() -> void:
 	if multiplayer.get_unique_id() != MultiplayerManager.get_driver_id():
-		freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
+		# freeze_mode = RigidBody3D.FREEZE_MODE_KINEMATIC
 		freeze = true
 
 func _process(delta: float) -> void:
-	camera_arm.position = position + Vector3.UP * 2
+	camera_arm.position = camera_arm.position.move_toward(position + Vector3.UP * 2, delta * 100)
 	var RPM_left = abs(rear_left_wheel.get_rpm())
 	var RPM_right = abs(rear_right_wheel.get_rpm())
 	
@@ -30,5 +33,12 @@ func _process(delta: float) -> void:
 		steering = lerp(steering, steering_direction * TURN_AMOUNT, TURN_SPEED * delta)
 
 		if direction == 0: brake = 2
+		network_transform = global_transform
+		network_quaternion = quaternion
+	else:
+		# global_transform = network_transform
+		# quaternion = network_quaternion
+		global_transform = global_transform.interpolate_with(network_transform, 0.1)
+		quaternion = quaternion.slerp(network_quaternion, 0.1)
 
 	rear_left_gpu_particles.emitting = rear_left_wheel.is_in_contact() and (brake > 0 or engine_force < 0) and RPM_left > 5

ced6a53690924d75d1d86e81337fc8f7ae0b3952
Riccardo Gabriele <gabririchi@yahoo.it>
2025-11-02 23:34:54 +0100

    Blood emitter and refactored weapons

---
+++ Components/HurtBox/HurtBox.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+class_name HurtBoxComponent
+
+signal on_hurt(weapon: Weapon, position: Vector3)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
---
+++ Weapon/Weapon.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name Weapon
+
+@export var damage: float = 1.0
+@export var attack_cooldown: float = 0.5
+@export var projectile_speed: int = 200
